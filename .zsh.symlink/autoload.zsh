fd722fe8afb1f01d4548ea028b5e4ba420c44fcb
Ali Sinan Köksal <alisinan@gmail.com>
2014-07-26 00:11:59 -0700

    history autocompetion with up and down arrow keys

--- .zsh.symlink/autoload.zsh
+++ .zsh.symlink/autoload.zsh
@@ -1,1 +1,6 @@
 autoload -U zmv
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
--- .zsh.symlink/bindkeys.zsh
+++ .zsh.symlink/bindkeys.zsh
@@ -1,1 +1,7 @@
 bindkey -v
+
+# for history autocompletion
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search]
