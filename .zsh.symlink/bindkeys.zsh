a4c9aadcbdee09c11429a7107bfef1da124c6c68
Ali Sinan Köksal <alisinan@gmail.com>
2014-10-03 11:17:15 -0700

    zsh history bindkey

--- .zsh.symlink/bindkeys.zsh
+++ .zsh.symlink/bindkeys.zsh
@@ -1,7 +1,6 @@
 bindkey -v
 
 # for history autocompletion
-bindkey '\eOA' up-line-or-beginning-search
-bindkey '\e[A' up-line-or-beginning-search
-bindkey '\eOB' down-line-or-beginning-search
-bindkey '\e[B' down-line-or-beginning-search]
+bindkey '' up-line-or-beginning-search
+bindkey 'OA' up-line-or-beginning-search
+bindkey 'OB' down-line-or-beginning-search
