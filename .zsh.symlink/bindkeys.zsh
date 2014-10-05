bd7561c31145466a8c2da89921de7ab6fd07ee17
Ali Sinan Köksal <alisinan@gmail.com>
2014-10-04 18:06:00 -0700

    tmux correct bindkeys

--- .zsh.symlink/bindkeys.zsh
+++ .zsh.symlink/bindkeys.zsh
@@ -1,6 +1,5 @@
 bindkey -v
 
 # for history autocompletion
-bindkey '' up-line-or-beginning-search
-bindkey 'OA' up-line-or-beginning-search
-bindkey 'OB' down-line-or-beginning-search
+bindkey "^K" up-line-or-beginning-search
+bindkey "^J" down-line-or-beginning-search
